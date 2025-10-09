5ce468e79a84041e299c454fdb4670e4ffd484a7
Gustavo Barboza <gustabarboza37@gmail.com>
2025-10-09 16:24:19 -0300

    Implementando o toDomain no ContactMapper

--- fe/src/services/ContactsService.js
+++ fe/src/services/ContactsService.js
@@ -8,24 +8,30 @@ class ContactsServices {
   }
 
   // Lista todos os contatos
-  listContacts(orderBy = 'asc') {
-    return this.httpClient.get(`/contacts/?orderBy=${orderBy}`);
+  async listContacts(orderBy = 'asc') {
+    const contacts = await this.httpClient.get(`/contacts/?orderBy=${orderBy}`);
+
+    return contacts.map((contact) => ContactMapper.toDomain(contact));
   }
 
   // Lista o contato com base no id
   async getContactById(id) {
     // await delay(500);
-    return this.httpClient.get(`/contacts/${id}`);
+    const contact = await this.httpClient.get(`/contacts/${id}`);
+
+    return ContactMapper.toDomain(contact);
   }
 
   // Cria um novo contato
   createContacts(contact) {
     const body = ContactMapper.toPersistence(contact);
+
     return this.httpClient.post('/contacts', { body });
   }
 
   updateContact(id, contact) {
     const body = ContactMapper.toPersistence(contact);
+
     return this.httpClient.put(`/contacts/${id}`, { body });
   }
 
