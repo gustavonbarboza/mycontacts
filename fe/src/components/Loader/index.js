0d53134c097ea8e7b349563e711f14d5f4ee99ca
Gustavo Barboza <gustabarboza37@gmail.com>
2026-01-14 18:51:35 -0300

    Adicionado o hook useAnimatedUnmount no Loader (Efeito de saida e entrada)

--- fe/src/components/Loader/index.js
+++ fe/src/components/Loader/index.js
@@ -4,14 +4,18 @@ import { Overlay } from './styles';
 import Spinner from '../Spinner';
 import ReactPortal from '../ReactPortal';
 
+import useAnimatedUnmount from '../../hooks/useAnimatedUnmount';
+
 export default function Loader({ isLoading }) {
-  if (!isLoading) {
+  const { sholdRender, animatedElementRef } = useAnimatedUnmount(isLoading);
+
+  if (!sholdRender) {
     return null;
   }
 
   return (
     <ReactPortal containerId="loader-root">
-      <Overlay>
+      <Overlay isLeaving={!isLoading} ref={animatedElementRef}>
         <Spinner size={90} />
       </Overlay>
     </ReactPortal>
