68a07275f53db7f5b73b34ed4a4809bc50ea20ca
Gustavo Barboza <gustabarboza37@gmail.com>
2026-01-19 17:45:44 -0300

    Criando as referências com o createRef

--- fe/src/hooks/useAnimatedList.js
+++ fe/src/hooks/useAnimatedList.js
@@ -1,8 +1,12 @@
-import { useState, useCallback } from 'react';
+import {
+  useState, useCallback, useRef, createRef,
+} from 'react';
 
 export default function useAnimatedList(inicialValue = []) {
   const [items, setItems] = useState(inicialValue);
   const [pedingRemovalItemsIds, setPedingRemovalItemsIds] = useState([]);
+
+  const animatedRefs = useRef(new Map());
 
   const handleRemoveItem = useCallback((id) => {
     setPedingRemovalItemsIds(
@@ -10,24 +14,38 @@ export default function useAnimatedList(inicialValue = []) {
     );
   }, []);
 
-  const handleAnimationEnd = useCallback((id) => {
-    setItems((prevState) => prevState.filter((item) => item.id !== id));
-    setPedingRemovalItemsIds(
-      (prevState) => prevState.filter((itemId) => itemId !== id),
-    );
+  // const handleAnimationEnd = useCallback((id) => {
+  //   setItems((prevState) => prevState.filter((item) => item.id !== id));
+  //   setPedingRemovalItemsIds(
+  //     (prevState) => prevState.filter((itemId) => itemId !== id),
+  //   );
+  // }, []);
+
+  const getAnimatedRef = useCallback((itemId) => {
+    let animatedRef = animatedRefs.current.get(itemId);
+
+    if (!animatedRef) {
+      animatedRef = createRef();
+      animatedRefs.current.set(itemId, animatedRef);
+    }
+
+    return animatedRef;
   }, []);
 
   const renderList = useCallback((renderItem) => (
-    items.map((item) => renderItem(item, {
-      isLeaving: pedingRemovalItemsIds.includes(item.id),
-    }))
-  ), [items, pedingRemovalItemsIds]);
+    items.map((item) => {
+      const isLeaving = pedingRemovalItemsIds.includes(item.id);
+
+      const animatedRef = getAnimatedRef(item.id);
+
+      return renderItem(item, { isLeaving, animatedRef });
+    })
+  ), [items, pedingRemovalItemsIds, getAnimatedRef]);
 
   return {
     items,
     setItems,
     handleRemoveItem,
-    handleAnimationEnd,
     renderList,
   };
 }
