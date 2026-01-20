99585df738aa646e14a806dba7280d10284b7c8f
Gustavo Barboza <gustabarboza37@gmail.com>
2026-01-20 16:21:38 -0300

    Adicionando os listeners de animationend

--- fe/src/hooks/useAnimatedList.js
+++ fe/src/hooks/useAnimatedList.js
@@ -1,5 +1,6 @@
 import {
   useState, useCallback, useRef, createRef,
+  useEffect,
 } from 'react';
 
 export default function useAnimatedList(inicialValue = []) {
@@ -7,19 +8,51 @@ export default function useAnimatedList(inicialValue = []) {
   const [pedingRemovalItemsIds, setPedingRemovalItemsIds] = useState([]);
 
   const animatedRefs = useRef(new Map());
+  const animationEndListeners = useRef(new Map());
+
+  const handleAnimationEnd = useCallback((itemId) => {
+    const removeListener = animationEndListeners.current.get(itemId);
+    removeListener();
+
+    animationEndListeners.current.delete(itemId);
+    animatedRefs.current.delete(itemId);
+
+    setItems((prevState) => prevState.filter((item) => item.id !== itemId));
+    setPedingRemovalItemsIds(
+      (prevState) => prevState.filter((id) => itemId !== id),
+    );
+  }, []);
+
+  useEffect(() => {
+    pedingRemovalItemsIds.forEach((itemId) => {
+      const animatedRef = animatedRefs.current.get(itemId);
+      const animatedElement = animatedRef?.current;
+      const alreadyHasListener = animationEndListeners.current.has(itemId);
+
+      if (animatedElement && !alreadyHasListener) {
+        const onAnimationEnd = () => handleAnimationEnd(itemId);
+        const removeListener = () => {
+          animatedElement.removeEventListener('animationend', onAnimationEnd);
+        };
+
+        animatedElement.addEventListener('animationend', onAnimationEnd);
+        animationEndListeners.current.set(itemId, removeListener);
+      }
+    });
+  }, [pedingRemovalItemsIds, handleAnimationEnd]);
+
+  useEffect(() => {
+    const removeListeners = animationEndListeners.current;
+    return () => {
+      removeListeners.forEach((removeListener) => removeListener());
+    };
+  }, []);
 
   const handleRemoveItem = useCallback((id) => {
     setPedingRemovalItemsIds(
       (prevState) => [...prevState, id],
     );
   }, []);
-
-  // const handleAnimationEnd = useCallback((id) => {
-  //   setItems((prevState) => prevState.filter((item) => item.id !== id));
-  //   setPedingRemovalItemsIds(
-  //     (prevState) => prevState.filter((itemId) => itemId !== id),
-  //   );
-  // }, []);
 
   const getAnimatedRef = useCallback((itemId) => {
     let animatedRef = animatedRefs.current.get(itemId);
